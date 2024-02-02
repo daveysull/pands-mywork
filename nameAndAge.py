e1466e32659d340e06b2d1807fcffcc429a16673
dave <you@example.com>
2024-02-02 15:40:48 +0000

    Practise

--- nameAndAge.py
+++ nameAndAge.py
@@ -1,3 +1,5 @@
 name = input("Enter your Name ")
 age = input("Enter your age ")
-print('Hello ' + name +","+ '\tyour age is ' + age)
+#print('Hello ' + name +","+ '\tyour age is ' + age)
+
+print(f'Hello my name is {name} and I am {age}')
